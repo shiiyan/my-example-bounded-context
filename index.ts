cf6dfcac91efc37746c849d282c3e4598aa819f4
yan <yan39123@gmail.com>
2023-06-30 10:33:29 +0900

    feat: add index router

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import { router } from "./routes/index";
 
 const app: Express = express();
 
-app.get("/", async (_req: Request, res: Response) => {
-  return res.status(200).send("Hello World!");
-});
+app.use("/", router);
 
 app.listen("8000", () => {
   console.log("dev server running at: http://localhost:8000");
---
+++ routes/index.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from "express";
+
+export const router = express.Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
