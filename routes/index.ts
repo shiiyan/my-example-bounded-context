18ace573202cb2e69d1d8d66962d9961f9a6ebf1
shiiyan <yan39123@gmail.com>
2023-07-01 16:27:00 +0900

    feat: add route /users to router

--- routes/index.ts
+++ routes/index.ts
@@ -5,3 +5,7 @@ export const router = express.Router();
 router.get("/", async (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+router.get("/users", async (req: Request, res: Response) => {
+  res.send("Hello users");
+});
