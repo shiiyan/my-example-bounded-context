583894dd6819fc3aa2323cf57173337518898d27
shiiyan <yan39123@gmail.com>
2023-07-31 08:14:20 +0900

    test: add test role application service

--- boundedContexts/iam/tests/application/roleApplicationService.test.ts
+++ boundedContexts/iam/tests/application/roleApplicationService.test.ts
@@ -1,1 +1,14 @@
-test();
+import { UserInRoleDto } from "@iam/main/application/dto/userInRoleDto";
+import { UserApplicationService } from "@iam/main/application/roleApplicationService";
+
+test("when get user with role tester, then user id will be returned", () => {
+  const sut = new UserApplicationService();
+  const actual = sut.getUserInRole({ id: "12345", roleName: "tester" });
+  expect(actual?.id.value).toBe("12345");
+});
+
+test("when get user with role tester, then user name will be returned", () => {
+  const sut = new UserApplicationService();
+  const actual = sut.getUserInRole({ id: "12345", roleName: "tester" });
+  expect(actual?.name).toBe("");
+});
