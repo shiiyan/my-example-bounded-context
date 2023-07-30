754ce680395ace37841d2f4631d307dfce4e0007
shiiyan <yan39123@gmail.com>
2023-07-30 12:30:44 +0900

    chore: config moduleNameMapper in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@collaboration/(.*)$": "<rootDir>/boundedContexts/collaboration/$1",
+    "^@iam/(.*)$": "<rootDir>/boundedContexts/iam/$1",
+    "^@common/(.*)$": "<rootDir>/boundedContexts/common/$1",
+  },
 };
