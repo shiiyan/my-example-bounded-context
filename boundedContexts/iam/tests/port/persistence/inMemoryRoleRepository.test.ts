651003fd3c75c1f161b4962a9ea9e814c5e17ea6
shiiyan <yan39123@gmail.com>
2023-08-02 08:40:31 +0900

    test: new Role in InMemoryRoleRepositoryTest

--- boundedContexts/iam/tests/port/persistence/inMemoryRoleRepository.test.ts
+++ boundedContexts/iam/tests/port/persistence/inMemoryRoleRepository.test.ts
@@ -1,17 +1,26 @@
+import { UUID } from "@common/domain/uuid";
 import { Role } from "@iam/main/domain/access/role";
+import { Group } from "@iam/main/domain/identity/group";
 import { InMemoryRoleRepository } from "@iam/main/port/persistence/inMemoryRoleRepository";
 
 test("save role", () => {
-  const role = Role.createNew({ name: "tester" });
+  const role = new Role({
+    id: UUID.createNew(),
+    name: "tester",
+    group: Group.createNew({ name: "tester" }),
+  });
   new InMemoryRoleRepository().save(role);
 });
 
-
 test("when save role, then role is found by name", () => {
-    const role = Role.createNew({ name: "tester" });
-    const sut = new InMemoryRoleRepository();
-    sut.save(role);
+  const role = new Role({
+    id: UUID.createNew(),
+    name: "tester",
+    group: Group.createNew({ name: "tester" }),
+  });
+  const sut = new InMemoryRoleRepository();
+  sut.save(role);
 
-    const actual = sut.findByName("tester");
-    expect(actual).toBeInstanceOf(Role);
+  const actual = sut.findByName("tester");
+  expect(actual).toBeInstanceOf(Role);
 });
