658eaff878a6fa86a4013cacfc5894cda77fe042
shiiyan <yan39123@gmail.com>
2023-08-02 09:10:08 +0900

    test: move role to before each in role test

--- boundedContexts/iam/tests/domain/role.test.ts
+++ boundedContexts/iam/tests/domain/role.test.ts
@@ -1,15 +1,25 @@
+import { UUID } from "@common/domain/uuid";
 import { Role } from "@iam/main/domain/access/role";
+import { Group } from "@iam/main/domain/identity/group";
 import { User } from "@iam/main/domain/identity/user";
 
+let role: Role;
+
+beforeEach(() => {
+  role = new Role({
+    id: UUID.createNew(),
+    name: "tester",
+    group: Group.createNew({ name: "tester" }),
+  });
+});
+
 test("assign user to role", () => {
-  const role = new Role({ name: "tester" });
   const user = User.createNew({ userName: "A", emailAddress: "a@example.com" });
 
   role.assignUser(user);
 });
 
 test("when assign user to role, then the user is in role", () => {
-  const role = new Role({ name: "tester" });
   const user = User.createNew({ userName: "A", emailAddress: "a@example.com" });
   role.assignUser(user);
 
@@ -17,7 +27,6 @@ test("when assign user to role, then the user is in role", () => {
 });
 
 test("when assign user to role, then another user with same name is not in role", () => {
-  const role = new Role({ name: "tester" });
   const user = User.createNew({ userName: "A", emailAddress: "a@example.com" });
   role.assignUser(user);
 
@@ -30,7 +39,6 @@ test("when assign user to role, then another user with same name is not in role"
 });
 
 test("when assign user to role, then another user with same email address is not in role", () => {
-  const role = new Role({ name: "tester" });
   const user = User.createNew({ userName: "A", emailAddress: "a@example.com" });
   role.assignUser(user);
 
@@ -43,7 +51,6 @@ test("when assign user to role, then another user with same email address is not
 });
 
 test("when assign user to role twice, then throw error", () => {
-  const role = new Role({ name: "tester" });
   const user = User.createNew({ userName: "A", emailAddress: "a@example.com" });
   role.assignUser(user);
 
